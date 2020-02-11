7b3687cffffbf279088037eb09e0496246759566
Raj Prajapati <raj@Rajs-MacBook-Pro.local>
2020-02-11 16:35:05 +0530

    token_sales2

---
+++ test/truffle.js
@@ -0,0 +1,11 @@
+module.export = {
+ 
+  networks: {
+  	development: {
+  		host: "127.0.0.1",     // Localhost (default: none)
+         port: 7545,            // Standard Ethereum port (default: none)
+        network_id: "*",
+  	}
+  }
+
+};
